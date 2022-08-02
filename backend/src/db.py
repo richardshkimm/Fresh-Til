9f876a799a24b1d1660f62d0094abe310a29e155
richardshkimm <richardshkim04@gmail.com>
2022-08-02 15:24:36 -0400

    Create backend folder

---
+++ backend/src/db.py
@@ -0,0 +1,10 @@
+import sqlite3
+
+class DatabaseDriver(object):
+    """
+    Database driver for the Venmo (Full) app.
+    Handles with reading and writing data with the database.
+    """
+
+    def __init__(self):
+        pass
